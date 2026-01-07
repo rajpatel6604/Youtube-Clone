f5380f50be736761c0cd58516f2dea030b0f24d9
patelraj6604 <rpatel11032004@gmail.com>
2026-01-07 14:36:58 +0530

    all setup done

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,9 @@
+import asyncHandler from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res, next) => {
+  return res
+    .status(200)
+    .json({ success: true, message: "User Registered Successfully" });
+});
+
+export { registerUser };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,9 @@
 const asyncHandler = (requestHandler) => {
-    return
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
 };
 export default asyncHandler;
 
